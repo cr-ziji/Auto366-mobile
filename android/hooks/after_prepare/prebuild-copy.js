e561977f8834f44b6d760f97da888a79351933a1
cyrilguocode <cyril_0614@hotmail.com>
2026-05-03 13:25:18 +0800

    加入启动屏(目前浮窗和答案解析还有问题)

--- android/hooks/after_prepare/prebuild-copy.js
+++ android/hooks/after_prepare/prebuild-copy.js
@@ -6,9 +6,9 @@ var path = require('path');
 module.exports = function(context) {
     var rootdir = context.opts.projectRoot;
     var srcFile = path.join(rootdir, 'src', 'FlipbookScanner.java');
+    var splashSrc = path.join(rootdir, 'src', 'drawable', 'ic_cdv_splashscreen.xml');
     var platformDir = path.join(rootdir, 'platforms', 'android', 'app', 'src', 'main');
     var destDir = path.join(platformDir, 'java', 'com', 'auto366', 'flipbook');
-    var configXmlPath = path.join(platformDir, 'res', 'xml', 'config.xml');
 
     if (!fs.existsSync(srcFile)) {
         console.log('Warning: FlipbookScanner.java not found in src/');
@@ -23,6 +23,7 @@ module.exports = function(context) {
     fs.copyFileSync(srcFile, destFile);
     console.log('Copied: FlipbookScanner.java -> ' + path.relative(platformDir, destFile));
 
+    var configXmlPath = path.join(platformDir, 'res', 'xml', 'config.xml');
     if (fs.existsSync(configXmlPath)) {
         var configContent = fs.readFileSync(configXmlPath, 'utf8');
         if (!configContent.includes('FlipbookScanner')) {
@@ -31,5 +32,12 @@ module.exports = function(context) {
             fs.writeFileSync(configXmlPath, configContent);
             console.log('Registered: FlipbookScanner in platforms config.xml');
         }
+    }
+
+    var splashDestDir = path.join(platformDir, 'res', 'drawable');
+    if (fs.existsSync(splashSrc) && fs.existsSync(splashDestDir)) {
+        var splashDest = path.join(splashDestDir, 'ic_cdv_splashscreen.xml');
+        fs.copyFileSync(splashSrc, splashDest);
+        console.log('Copied: ic_cdv_splashscreen.xml -> ' + path.relative(platformDir, splashDest));
     }
 };
